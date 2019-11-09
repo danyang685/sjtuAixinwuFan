edc6377a4e6b96538a5a20058fe3433b11220fcb
danyang685 <496298685@qq.com>
2019-11-09 11:20:18 +0800

    用lxml改写html解析部分

--- aixinwuChecker.py
+++ aixinwuChecker.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
-from time import time,sleep
+from time import time, sleep
 from requests import session
-from bs4 import BeautifulSoup
+
+from lxml import etree
 import re
 import csv
 
-import smtplib
-from email.message import EmailMessage
+# import smtplib
+# from email.message import EmailMessage
+# from datetime import datetime
+
 import sched
-from datetime import datetime
 import random
 
 
-import socket
-import socks
+# import socket
+# import socks
 
 # # 前段时间服务器被墙了，于是就只能这样子了
 # socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 1080)
@@ -24,6 +26,7 @@ import socks
 document.cookie.split('; ').forEach(function(c){if(c.includes('JASiteCookie'))console.error(c)})
 回车执行，红字部分即为cookies中JASiteCookie字段数据，记录了在爱心屋网站的登陆状态。将红字部分粘贴到此处，即开通自动登录服务。风险提醒，提供JASiteCookie字段表示此次登陆状态将与我共享。
 '''
+
 class AixinwuChecker:
     headers = {
         'Accept': '*/*',
@@ -36,10 +39,11 @@ class AixinwuChecker:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
     }
     url_page = 'https://aixinwu.sjtu.edu.cn/index.php/login'
-    url_infoPage='https://aixinwu.sjtu.edu.cn/index.php/customer/info'
-    url_consignee='https://aixinwu.sjtu.edu.cn/index.php/customer/consignee'
-    strTemplate="{}({})同学，你已经连续登录爱心屋{}天了，爱心币余额{:.2f}，加油！"
-    def __init__(self,cookie):
+    url_infoPage = 'https://aixinwu.sjtu.edu.cn/index.php/customer/info'
+    url_consignee = 'https://aixinwu.sjtu.edu.cn/index.php/customer/consignee'
+    strTemplate = "{}({})同学，你已经连续登录爱心屋{}天了，爱心币余额{:.2f}，加油！"
+
+    def __init__(self, cookie):
         cookies = {'JASiteCookie': cookie}
         self.sess = session()
         self.sess.headers.update(self.headers)
@@ -47,85 +51,110 @@ class AixinwuChecker:
 
     def Login(self):
         response = self.sess.get(self.url_page)
-        soup = BeautifulSoup(response.content, 'lxml')
-        userRaw = soup.find("a",{"id": "userName"})
+        documentRoot = etree.HTML(response.text)
+        userRaw = documentRoot.xpath('//a[@id="userName"]')
         if not userRaw:
             return "连接失败"
-        
-        infos = soup.find_all("div",{"class": "header_userInfo_word"})
-        
-        userName=''
 
-        response =self.sess.get(self.url_infoPage)
-        soup = BeautifulSoup(response.content, 'lxml')
-        userNameElement = soup.find("input",{"id": "consignee"})
+        # 提取姓名
+        userName = ''
+        response = self.sess.get(self.url_infoPage)
+        documentRoot = etree.HTML(response.text)
+        userNameElement = documentRoot.xpath('//input[@id="consignee"]/@value')
         if userNameElement:
-            userName=userNameElement["value"].strip()
-        
-        userID=''
-        response =self.sess.get(self.url_consignee)
-        soup = BeautifulSoup(response.content, 'lxml')
-        consigneeRow = soup.find("tr",{"class": "thead-tbl-address default-address"})
-        
+            userName = userNameElement[0].strip()
+
+        # 提取学号
+        userID = ''
+        response = self.sess.get(self.url_consignee)
+        documentRoot = etree.HTML(response.text)
+        consigneeRow = documentRoot.xpath(
+            '//tr[@class="thead-tbl-address default-address"]')
         if consigneeRow:
-            userID=list(consigneeRow.find_all('td'))[1].text.strip()
-            
-        day=''
+            userID = consigneeRow[0].xpath('descendant::td/text()')[1].strip()
+
+        # 提取余额
+        day = ''
+        infos = documentRoot.xpath('//div[@class="header_userInfo_word"]')
         for i in infos:
             if '您已连续登陆' in i.text:
-                day=re.sub(r'[^0-9]', '', i.text)
+                day = re.sub(r'[^0-9]', '', i.text)
                 break
-        info = list(soup.find("ul",{"class": "header_fixedBox header_userInfo_box"}).find_all("li"))[0]
-        money=float(re.sub(r'[^0-9.]', '', info.text))
+        info = documentRoot.xpath(
+            '//ul[@class="header_fixedBox header_userInfo_box"][1]/li/text()')[0]
+        money = float(re.sub(r'[^0-9.]', '', info))
         if userName:
-            return str.format(self.strTemplate,userName,userID,day,money)
+            return str.format(self.strTemplate, userName, userID, day, money)
 
     def __call__(self):
         pass
 
+
 def registerAgain(scheduleObj):
-    scheduleObj.enter(random.randint(60*60*18,60*60*22), 0, doOper, (scheduleObj,))
+    scheduleObj.enter(random.randint(60*60*18, 60*60*22),
+                      0, doOper, (scheduleObj,))
     scheduleObj.run()
 
-    
+
 def doOper(scheduleObj):
-    replyStr=[]
+    replyStr = []
     with open('aixinwu.csv') as f:
-        reader=csv.reader(f)
+        reader = csv.reader(f)
         for item in reader:
-            if len(item)!=2:
+            if len(item) != 2:
                 continue
             user = AixinwuChecker(item[1])
-            replyStr.append(str.format("正尝试使用数据{}连接",item[0]))
+            replyStr.append(str.format("正尝试使用数据{}连接", item[0]))
             print(user.Login())
             replyStr.append(user.Login())
-            sleep(random.randint(1,2))
+            sleep(random.randint(1, 2))
     registerAgain(scheduleObj)
-    
+
+
 def parallel():
     from queue import SimpleQueue
     from threading import Thread
+
     def runner(userItem):
         user = AixinwuChecker(userItem[1])
-        replyStr.put(str.format("正尝试使用数据{}连接",item[0]))
-        replyStr.put(item[0]+user.Login())
+        replyStr.put(str.format("正尝试使用数据{}连接", userItem[0]))
+        replyStr.put(userItem[0]+user.Login())
+
     def printer():
         while 1:
-            print(replyStr.get())
-    replyStr=SimpleQueue()
+            newStr = replyStr.get()
+            if newStr:
+                print(newStr)
+            else:
+                break
+    replyStr = SimpleQueue()
+    threadList = []
     with open('aixinwu.csv') as f:
-        reader=csv.reader(f)
+        reader = csv.reader(f)
         Thread(target=printer).start()
         for item in reader:
-            if len(item)!=2:
+            if len(item) != 2:
                 continue
-            Thread(target=runner,args=(item,)).start()
+            threadList.append(Thread(target=runner, args=(item,)))
+    list(map(lambda thread: thread.start(), threadList))
+    list(map(lambda thread: thread.join(), threadList))
+    replyStr.put(None)
 
+
+def oneTest():
+    with open('aixinwu.csv') as f:
+        reader = csv.reader(f)
+        for userItem in reader:
+            if len(userItem) != 2:
+                continue
+            user = AixinwuChecker(userItem[1])
+            print(str.format("正尝试使用数据{}连接", userItem[0]))
+            print(userItem[0]+user.Login())
 
 
 if __name__ == '__main__':
     from sys import argv
-    #print(argv)
+    # print(argv)
 
     # # 周期执行模式
     # schedule = sched.scheduler(time, sleep)
@@ -134,3 +163,6 @@ if __name__ == '__main__':
 
     # 并行单次执行模式
     parallel()
+
+    # 测试模式
+    # oneTest()
